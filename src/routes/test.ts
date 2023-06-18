9385062ed72c3e3aeae10130a11bef742756b377
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-06-18 19:42:25 +0100

    Added more stuff to test

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -3,5 +3,5 @@ export const method = "GET";
 export const name = "test";
 
 export const run = async (request: FastifyRequest, reply: FastifyReply, cache: any) => {
-	reply.code(200).send({ message: "active" });
+	reply.code(200).send({ message: "Thermite server is active", activeScripts: Object.keys(cache).length });
 };
