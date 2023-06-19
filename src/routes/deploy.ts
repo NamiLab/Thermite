3f59c94b2fc3fcfd6b76288ea53da6993019341a
WaviestBalloon <edwardastill@hotmail.co.uk>
2023-06-19 18:27:07 +0100

    webhooks

--- src/routes/deploy.ts
+++ src/routes/deploy.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { randomUUID } from "crypto";
+import axios from "axios";
 export const method = "POST";
 export const name = "deploy";
 
@@ -8,14 +9,57 @@ import { createLua } from "../utils/CreateLua.js";
 export const run = async (request: FastifyRequest, reply: FastifyReply, cache: any) => {
 	if (!request.body) return reply.code(400).send({ error: "No body provided" });
 	//@ts-ignore
-	let burnTime = request.query?.burntime;
+	let burnTime = request.query?.burnTime;
 	if (!burnTime) burnTime = 120000;
 	if (burnTime > 600000) burnTime = 600000;
 
 	const scriptId = randomUUID();
-	cache[scriptId] = request.body;
+	cache[scriptId] = {
+		script: request.body,
+		burnTime: burnTime,
+		//@ts-ignore
+		webhook: request.query?.webhookUrl ? request.query?.webhookUrl : null,
+	};
 	console.log(`Deployed script #${Object.keys(cache).length - 1} - Cache size: ${Object.keys(cache).length} items stored`);
+	
+	if (cache[scriptId].webhook) {
+		axios.post(cache[scriptId].webhook, {
+			embeds: [
+				{
+					title: "Deployed to Thermite",
+					description: `Script (\`${scriptId}\`) was deployed <t:${Math.floor(Date.now() / 1000)}:R>, it will become unusable <t:${Math.floor((Date.now() + burnTime) / 1000)}:R> seconds.\n\nYou can use it by sending a GET request to the endpoint: \`/api/use/${scriptId}\``,
+					color: 9109357,
+					footer: {
+						text: "Thermite",
+						icon_url: "https://github.com/NamiLab/Thermite/blob/main/gitassets/therm2.png?raw=true"
+					}
+				}
+			],
+		}).catch((err) => {
+			console.warn(`Failed to send webhook: ${err}`);
+		});
+	}
+	
 	setTimeout(() => {
+		if (!cache[scriptId]) return;
+
+		if (cache[scriptId].webhook) {
+			axios.post(cache[scriptId].webhook, {
+				embeds: [
+					{
+						title: "Burned script",
+						description: `Script (\`${scriptId}\`) <t:${Math.floor(Date.now() / 1000)}:R> was burned as it's burn time has passed.`,
+						color: 16739693,
+						footer: {
+							text: "Thermite",
+							icon_url: "https://github.com/NamiLab/Thermite/blob/main/gitassets/therm2.png?raw=true"
+						}
+					}
+				],
+			}).catch((err) => {
+				console.warn(`Failed to send webhook: ${err}`);
+			});
+		}
 		delete cache[scriptId];
 		console.log(`Script #${scriptId} was auto-purged - Cache size: ${Object.keys(cache).length} items stored`);
 	}, burnTime);
--- src/routes/use.ts
+++ src/routes/use.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import axios from "axios";
 export const method = "GET";
 export const name = "use/:id";
 
@@ -6,11 +7,53 @@ export const run = async (request: FastifyRequest, reply: FastifyReply, cache: a
 	//@ts-ignore
 	const id = request.params?.id;
 	if (!id) return reply.code(400).send({ error: "Invalid Script ID" });
+	if (request.headers["user-agent"] != "Roblox Corporation") { // TODO: https://www.npmjs.com/package/netmask | https://bgp.he.net/AS22697#_prefixes
+		reply.code(403).send({ error: "Only Roblox gameinstances are allowed to use this endpoint" });
+		
+		if (cache[id].webhook) {
+			console.log(`Sending webhook for script #${id}`);
+			axios.post(cache[id].webhook, {
+				embeds: [
+					{
+						title: "Alert",
+						description: `Something tried to access your script (\`${id}\`) <t:${Math.floor(Date.now() / 1000)}:R>, the script is still accessible.`,
+						color: 16739693,
+						footer: {
+							text: "Thermite",
+							icon_url: "https://github.com/NamiLab/Thermite/blob/main/gitassets/therm2.png?raw=true"
+						}
+					}
+				],
+			}).catch((err) => {
+				console.warn(`Failed to send webhook: ${err}`);
+			});
+		}
+		return;
+	}
 	
 	let found = false;
 	for (const key in cache) {
 		if (key === id) {
-			reply.type("application/text").code(200).send(cache[key]);
+			reply.type("application/text").code(200).send(cache[key].script);
+			if (cache[key].webhook) {
+				console.log(`Sending webhook for script #${id}`);
+				axios.post(cache[id].webhook, {
+					embeds: [
+						{
+							title: "Alert",
+							description: `Your script (\`${id}\`) was accessed <t:${Math.floor(Date.now() / 1000)}:R> and is now no longer accessible.`,
+							color: 9109357,
+							footer: {
+								text: "Thermite",
+								icon_url: "https://github.com/NamiLab/Thermite/blob/main/gitassets/therm2.png?raw=true"
+							}
+						}
+					],
+				}).catch((err) => {
+					console.warn(`Failed to send webhook: ${err}`);
+				});
+			}
+
 			delete cache[key];
 			console.log(`Script #${id} was used up, deleted - Cache size: ${Object.keys(cache).length} items stored`);
 			found = true;
